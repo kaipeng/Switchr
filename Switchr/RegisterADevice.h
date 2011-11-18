01b83e78f16e1582b288b29a93542eb244b9449d
Jeff Kiske <Jeff@seas439.wlan.seas.upenn.edu>
2011-11-18 17:33:22 -0500

    Added a new view for 'register a device'

---
+++ Switchr/RegisterADevice.h
@@ -0,0 +1,13 @@
+//
+//  RegisterADevice.h
+//  Switchr
+//
+//  Created by Jeffrey Kiske on 11/18/11.
+//  Copyright (c) 2011 University Of Pennsylvania. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterADevice : UIViewController
+
+@end
---
+++ Switchr/RegisterADevice.m
@@ -0,0 +1,60 @@
+//
+//  RegisterADevice.m
+//  Switchr
+//
+//  Created by Jeffrey Kiske on 11/18/11.
+//  Copyright (c) 2011 University Of Pennsylvania. All rights reserved.
+//
+
+#import "RegisterADevice.h"
+
+@implementation RegisterADevice
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
